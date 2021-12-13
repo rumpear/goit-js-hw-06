f87410a0d0b11f8dc546917d8cb75e1c4ae137ef
Roman <77742141+rumpear@users.noreply.github.com>
2021-12-13 17:20:01 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,17 +1,20 @@
 const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
 ];
 
-const ingridientsListRef = document.querySelector("#ingredients");
+const ingridientsListRef = document.querySelector('#ingredients');
+
+const ingridientsArr = [];
 
 for (const ingredient of ingredients) {
-  const item = document.createElement("li");
+  const item = document.createElement('li');
   item.textContent = ingredient;
-  item.classList.add("item");
-  ingridientsListRef.append(item);
+  item.classList.add('item');
+  ingridientsArr.push(item);
 }
+ingridientsListRef.append(...ingridientsArr);
