7281e3328bad3abc3a9ff1bbdba730c87ba0aeac
Roman <77742141+rumpear@users.noreply.github.com>
2021-12-15 12:12:21 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
+const bodyRef = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', onClickChangeColorBody);
+
+function onClickChangeColorBody() {
+  // const a = (bodyRef.style.backgroundColor = getRandomHexColor());
+  // colorSpan.textContent = a;
+  // console.log((colorSpan.textContent = a));
+
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorSpan.textContent = bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
