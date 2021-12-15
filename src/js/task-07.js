b368270a4727adb22772cbc4e8d7231d4cb3dae9
Roman <77742141+rumpear@users.noreply.github.com>
2021-12-15 12:12:14 +0200

    Update task-07.js

---
+++ src/js/task-07.js
@@ -0,0 +1,8 @@
+const inputFontSizeControl = document.querySelector('#font-size-control');
+const textSpan = document.querySelector('#text');
+
+inputFontSizeControl.addEventListener('input', onInputFontSizeChange);
+
+function onInputFontSizeChange(event) {
+  textSpan.style.fontSize = event.currentTarget.value + 'px';
+}
