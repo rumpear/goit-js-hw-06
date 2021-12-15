ca2ec2c9d82e9f6c3941e1a8f3624d4dac62330b
Roman <77742141+rumpear@users.noreply.github.com>
2021-12-15 12:12:17 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,46 @@
+const loginFormRef = document.querySelector('.login-form');
 
+loginFormRef.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//   const {
+//     elements: { email, password },
+//   } = event.currentTarget;
+
+//   const formData = {
+//     email,
+//     password,
+//   };
+
+//   console.log(formData.email.value, formData.password.value);
+// }
+
+// console.log(loginFormRef);
+
+// * alert
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  const formData = {
+    email,
+    password,
+  };
+
+  if (formData.email.value === '' || formData.password.value === '') {
+    alert('Все поля должны быть заполнены');
+  } else {
+    console.log(
+      `The email is: ${formData.email.value}. The password is: ${formData.password.value}`
+    );
+    loginFormRef.reset();
+  }
+}
+
+// event.currentTarget.elements.email.value === '' ||
+// event.currentTarget.elements.password.value === ''
